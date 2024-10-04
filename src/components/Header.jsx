9d61faa339ac516680ae54e20a3fe87325b7b132
gantud <88612010+gantud@users.noreply.github.com>
2024-10-04 15:22:55 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header({cart, removeFromCart, increaseQuantity, decreaseQuantity, clear
             <div className="row justify-content-center justify-content-md-between">
                 <div className="col-8 col-md-3">
                     <a href="index.html">
-                        <img className="img-fluid" src="./img/logo.png" alt="imagen logo" />
+                        <img className="img-fluid" src="{`${process.env.PUBLIC_URL}/img/logo.png`}" alt="imagen logo" />
                     </a>
                 </div>
                 <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
